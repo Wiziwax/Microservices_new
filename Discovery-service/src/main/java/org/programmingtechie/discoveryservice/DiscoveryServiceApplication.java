ae92988d3c8df436f633d14b1edf202d3d1e609b
Wiziwax <donaldwisdomnengi@gmail.com>
2024-01-29 13:12:13 +0100

    Eureka Service

--- Discovery-service/src/main/java/org/programmingtechie/discoveryservice/DiscoveryServiceApplication.java
+++ Discovery-service/src/main/java/org/programmingtechie/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package org.programmingtechie.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
